57195de1eb7e80469ae5cc719c09e74c0abbcb2b
Fabio Bufalari <bufalari.fabio@gmail.com>
2025-05-21 22:57:03 -0300

    initial commit

---
+++ src/main/java/com/bufalari/financial_reports_service/FinancialReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bufalari.financial_reports_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialReportsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bufalari/financial_reports_service/FinancialReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufalari.financial_reports_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
